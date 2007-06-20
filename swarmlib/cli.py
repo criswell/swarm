56a8693ebc085335bb8f43d08bfb9624a5968383
sam <sam@rygel.home.samhart.com>
2007-06-20 11:22:45 -0400

    Verify that we have short opts to parse

--- swarmlib/cli.py
+++ swarmlib/cli.py
@@ -103,8 +103,9 @@ def cli_parse(argv):
             post_opt.append(option)
 
     short_opts = ''
-    for a in option_dispatch[command].short_opts:
-        short_opts = short_opts + a
+    if option_dispatch[command].short_opts:
+        for a in option_dispatch[command].short_opts:
+            short_opts = short_opts + a
 
     opts, args = getopt.getopt(pre_opt, short_opts, option_dispatch[command].long_opts)
 
