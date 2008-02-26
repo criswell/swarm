66c1af031e82f6d9bed03af0fe52455c01e8868b
sam <sam@rygel.home.samhart.com>
2008-02-26 15:19:01 -0500

    Whitespace police

--- swarmlib/clone.py
+++ swarmlib/clone.py
@@ -70,22 +70,21 @@ class clone:
 
         # add this clone to tbe upstream tracker
         self._add_tracker()
-        
+
         # Log the clone transaction
         # ERE I AM JH
         #self._dest_sw.db.backend.log_transaction(self._dest_sw.db.__MASTER_ISSUE__, 'clone', self._dest_sw.xactions.dispatch['clone'].encode(SOMETHING HERE))
 
         self._logger.unregister()
-        
 
     def _add_tracker(self):
         """
         add the linkage to the upstream tracker
         internal function. do not call
         """
-        
+
         self._logger.register('_add_tracker')
-        
+
         # heer i am jh
-        
+
         self._logger.unregister()
