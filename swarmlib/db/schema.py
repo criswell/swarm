9b21024d4fd0519496537af37ae60e66080ef9d6
Sam Hart <criswellious@gmail.com>
2008-04-02 16:45:56 -0400

    More work on sqlalchemy-based schema rewrite

--- swarmlib/db/schema.py
+++ swarmlib/db/schema.py
@@ -27,73 +27,91 @@ This package defines the database schema used in a Swarm Hive.
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, \
                        Float, PickleType, Text
 
-from sqlalchemy.orm import mapper
+from sqlalchemy.orm import mapper, relation, backref
 
-metadata = MetaData()
+from swarmlib.db import metadata
+
+#metadata = MetaData()
+# REMOVE
+#from sqlalchemy import create_engine
+#engine = create_engine('sqlite:///:memory:', echo=True)
+#metadata.create_all(engine)
 
 __HASH_ID_LENGTH__ = 40
 __USER_ID_LENGTH__ = 60
+__SUMMARY_LENGTH__ = 255
 
-issue_table = Table('issue', metadata,
-    Column('id', Integer, primary_key=True, autoincrement=True),
-    Column('short_hash_id',  String(__HASH_ID_LENGTH__), unique=True),
-    Column('hash_id', String(__HASH_ID_LENGTH__), unique=True),
+issues_table = Table('issues', metadata,
+    Column('hash_id', String(40), primary_key=True, unique=True, nullable=False),
+    Column('short_hash_id', String(40), unique=True, nullable=False),
 
     # XXX: The following all need to have default values. The best way
     # will probably be to set up a mapping in sqlalchemy orm, but I don't
     # yet know how best to do this, so I'm marking it as a FIXME
-    Column('component', Integer, nullable=False),
-    Column('version', Integer, nullable=False),
-    Column('milestone', Integer, nullable=False),
-    Column('severity', Integer, nullable=False),
-    Column('priority', Integer, nullable=False),
-    Column('status', Integer, nullable=False),
-    Column('resolution', Integer, nullable=False),
+    #Column('component', Integer, nullable=False),
+    #Column('version', Integer, nullable=False),
+    #Column('milestone', Integer, nullable=False),
+    #Column('severity', Integer, nullable=False),
+    #Column('priority', Integer, nullable=False),
+    #Column('status', Integer, nullable=False),
+    #Column('resolution', Integer, nullable=False),
 
-    Column('owner', String(__USER_ID_LENGTH__)),
-    Column('reporter', String(__USER_ID_LENGTH__), nullable=False),
+    #Column('owner', String(__USER_ID_LENGTH__)),
+    #Column('reporter', String(__USER_ID_LENGTH__), nullable=False),
 
     # FIXME XXX: More mappings will be needed here
-    Column('cc_id', Integer),
-    Column('subscribers_id', Integer),
+    #Column('cc_id', Integer),
+    #Column('subscribers_id', Integer),
 
-    Column('time', Float),
+    #Column('time', Float),
+)
+
+self.nodes_table = Table('nodes', metadata,
+    Column('hash_id', String(40), primary_key=True, unique=True, nullable=False),
+    Column('summary', String(100), nullable=False),
+    Column('issue_id', String(40), ForeignKey('issues.hash_id')),
+    Column('parent_node_id', String(40), ForeignKey('nodes.hash_id')),
+
+    #Column('time', Float),
+
+    #Column('poster', String(__USER_ID_LENGTH__)),
+
+    # FIXME XXX: Need to be mapped
+    #Column('related_id', Integer),
+
+    #Column('summary', String(__SUMMARY_LENGTH__)),
+    #Column('details', Text),
+
+    #Column('attachment_id', String(__HASH_ID_LENGTH__)),
 )
 
 class Issue(object):
-    # FIXME XXX: Is this how we want to do this?
-    pass
-
-node_table = Table('node', metadata,
-    Column('node_id', String(__HASH_ID_LENGTH__), unique=True),
-
-    Column('time', Float),
-
-    Column('poster', String(__USER_ID_LENGTH__)),
-
-    # FIXME XXX: Need to be mapped
-    Column('related_id', Integer),
-
-    Column('summary', Text),
-    Column('details', Text),
-
-    Column('attachment_id', String(__HASH_ID_LENGTH__)),
-
-    Column('root_issue_id', String(__HASH_ID_LENGTH__),
-            ForeignKey('issue.hash_id')),
-)
+    def __init__(self, hash_id, short_hash_id):
+        self.short_hash_id = short_hash_id
+        self.hash_id = hash_id
+    def __repr__(self):
+       return "<Issue('%s', '%s')>" % (self.hash_id, self.short_hash_id)
 
 class Node(object):
-    # FIXME XXX: See Issue above
-    pass
+    def __init__(self, hash_id, summary):
+        self.hash_id = hash_id
+        self.summary = summary
+    def __repr__(self):
+        return "<Node('%s', '%s')>" % (self.hash_id, self.summary)
 
-try:
-    mapper(Node, node_table, properties={
-        'parents':relation(Node, backref='node'),
-        'children':relation(Node, backref='node'),
-    })
-    mapper(Issue, issue_table, properties={
-        'root_node':relation(Node, uselist=False, backref='node'),
-    })
-except:
-    pass
+# REMOVE
+#metadata.create_all(engine)
+
+mapper(Issue, issues_table, properties={
+    'root_nodes':relation(Node) #, backref='issue')
+})
+mapper(Node, nodes_table, properties={
+    'children':relation(Node, backref=backref('parent', remote_side=[nodes_table.c.hash_id]))
+})
+
+
+# REMOVE
+
+#from sqlalchemy.orm import sessionmaker
+#Session = sessionmaker(bind=engine, autoflush=True, transactional=True)
+#session = Session()
