ec1661ea552147da905435faaf5e390557cb81de
Sam Hart <hartsn@gmail.com>
2011-01-29 10:32:52 -0800

    commiting what little I have and getting out of panara away from all the noise

---
+++ dochemistry/__init__.py
@@ -0,0 +1,12 @@
+"""
+@package dochemistry
+
+@mainpage Wrapper for various NoSQL, document-oriented databases
+
+@section Introduction Introduction
+  doChemistry is intended to be an abstraction layer for various NoSQL and
+document-oriented databases.
+
+"""
+
+__version__ = '0.0.1'
